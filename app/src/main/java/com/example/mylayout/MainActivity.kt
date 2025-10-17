c3ba183b171a5025e12dad704ddcb1c83bba7fd9
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-17 10:19:33 +0700

    menambah import Compose Layout, Material3, dan Theme

--- app/src/main/java/com/example/mylayout/MainActivity.kt
+++ app/src/main/java/com/example/mylayout/MainActivity.kt
@@ -3,3 +3,12 @@ package com.example.mylayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat.enableEdgeToEdge
+import com.example.mylayout.ui.theme.MyLayoutTheme
