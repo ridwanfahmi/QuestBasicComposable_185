cc9afa345e5dab25754a3af61eda4c426fb5978a
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-17 10:54:33 +0700

    membuat box kedua dengan warna cyan

--- app/src/main/java/com/example/mylayout/Tataletak.kt
+++ app/src/main/java/com/example/mylayout/Tataletak.kt
@@ -131,6 +131,15 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
             }
         }
         Spacer(modifier = Modifier.height(height = 10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+
+        }
     }
 }
 
