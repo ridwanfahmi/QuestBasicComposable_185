3d725cfd53cc14ae2d4a307e3c95c64fb7e7fc73
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-17 10:32:05 +0700

    menambahkan layout box sederhana

--- app/src/main/java/com/example/mylayout/Tataletak.kt
+++ app/src/main/java/com/example/mylayout/Tataletak.kt
@@ -44,3 +44,18 @@ fun TataLetakRow(modifier: Modifier) {
         Text(text = "Komponen4")
     }
 }
+
+@Composable
+fun TataLetakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
+}
