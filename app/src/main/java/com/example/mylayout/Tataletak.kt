32c5d6daa3b7a4b5c1cc681f3cf471d5e93900ae
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-17 10:49:48 +0700

    menambahkan row pertama didalam box kuning

--- app/src/main/java/com/example/mylayout/Tataletak.kt
+++ app/src/main/java/com/example/mylayout/Tataletak.kt
@@ -106,7 +106,21 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
                 .height(110.dp)
                 .background(color = Color.Yellow),
             contentAlignment = Alignment.Center
-        ) { }
+        )
+        {
+            Column() {
+
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
+                }
+
+            }
+        }
     }
 }
 
