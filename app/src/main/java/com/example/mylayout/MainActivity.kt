b7a6e1c915aa69af567916bb6f9f38aa0333b0f6
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-17 10:39:11 +0700

    memperbaiki import di mainactivity dan tataletak

--- app/src/main/java/com/example/mylayout/MainActivity.kt
+++ app/src/main/java/com/example/mylayout/MainActivity.kt
@@ -6,11 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat.enableEdgeToEdge
 import com.example.mylayout.ui.theme.MyLayoutTheme
 
 class MainActivity : ComponentActivity() {
