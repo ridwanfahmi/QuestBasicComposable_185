f21b39cbbbbc4a464f5d5e04e27c97625269f4a4
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-17 10:28:51 +0700

    menambahkan layout coloum sederhana

--- app/src/main/java/com/example/mylayout/Tataletak.kt
+++ app/src/main/java/com/example/mylayout/Tataletak.kt
@@ -23,3 +23,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@Composable
+fun TataLetakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
