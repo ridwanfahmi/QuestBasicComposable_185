f533c3ca585165ec7efaed48e772fe5300c04a83
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-17 10:35:40 +0700

    menambahkan kombinasi layout row dan coloum

--- app/src/main/java/com/example/mylayout/Tataletak.kt
+++ app/src/main/java/com/example/mylayout/Tataletak.kt
@@ -80,3 +80,23 @@ fun TataLetakColumnRow(modifier: Modifier) {
         }
     }
 }
+
+
+@Composable
+fun TataLetakRowColumn(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        //Kolom1
+        Column() {
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+        }
+
+        //Kolom2
+        Column() {
+            Text(text = "Komponen1Kolom2")
+            Text(text = "Komponen2Kolom2")
+            Text(text = "Komponen2Kolom2")
+        }
+    }
+}
